2b8b638110be2617412c78811d644ee87c028d55
Qualimiox <l.buhrmester@gmx.de>
2026-07-25 15:29:58 +0200

    Use current timeStamp for last_seen

--- src/features/auth/api/setUserTrainerName.ts
+++ src/features/auth/api/setUserTrainerName.ts
@@ -4,8 +4,8 @@ import { pool } from '@/database';
 
 export const setUserTrainerName = async (userId: string, trainerName: string): Promise<boolean> => {
   const [resultSetHeader] = await pool.execute(
-    'INSERT INTO `player` (`name`, `friendship_id`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `name` = VALUES(`name`), `friendship_id` = VALUES(`friendship_id`)',
-    [trainerName, userId],
+    'INSERT INTO `player` (`name`, `last_seen`, `friendship_id`) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE `name` = VALUES(`name`), `last_seen` = VALUES(`last_seen`), `friendship_id` = VALUES(`friendship_id`)',
+    [trainerName, Math.floor(Date.now() / 1000), userId],
   );
 
   return (resultSetHeader as ResultSetHeader).affectedRows >= 1;

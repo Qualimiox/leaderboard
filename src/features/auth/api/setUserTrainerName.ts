46b713741714fcc858a820109262394ec3113589
Qualimiox <l.buhrmester@gmx.de>
2026-07-25 14:53:57 +0200

    Fix table name when submitting new trainer

--- src/features/auth/api/setUserTrainerName.ts
+++ src/features/auth/api/setUserTrainerName.ts
@@ -3,10 +3,10 @@ import type { ResultSetHeader } from 'mysql2';
 import { pool } from '@/database';
 
 export const setUserTrainerName = async (userId: string, trainerName: string): Promise<boolean> => {
-  const [resultSetHeader] = await pool.execute('UPDATE `pogo_leaderboard_users` SET `trainerName` = ? WHERE id = ?;', [
-    trainerName,
-    userId,
-  ]);
+  const [resultSetHeader] = await pool.execute(
+    'INSERT INTO `player` (`name`, `friendship_id`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `name` = VALUES(`name`), `friendship_id` = VALUES(`friendship_id`)',
+    [trainerName, userId],
+  );
 
-  return (resultSetHeader as ResultSetHeader).affectedRows === 1;
+  return (resultSetHeader as ResultSetHeader).affectedRows >= 1;
 };
